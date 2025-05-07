e67c530da63465563a2a0f4f0667aaf444e00bb0
BellamyPhan <bellamyphan@icloud.com>
2025-05-06 21:16:17 -0500

    SchoolController: add new method findAllStudentsBySchoolId

--- src/main/java/com/bellamyphan/microservice_school/controller/SchoolController.java
+++ src/main/java/com/bellamyphan/microservice_school/controller/SchoolController.java
@@ -1,5 +1,6 @@
 package com.bellamyphan.microservice_school.controller;
 
+import com.bellamyphan.microservice_school.dto.SchoolDto;
 import com.bellamyphan.microservice_school.model.School;
 import com.bellamyphan.microservice_school.service.SchoolService;
 import lombok.RequiredArgsConstructor;
@@ -25,5 +26,10 @@ public class SchoolController {
     @GetMapping
     public ResponseEntity<List<School>> findAllSchools() {
         return ResponseEntity.ok(service.findAllSchools());
+    }
+
+    @GetMapping("/with-students/{school-id}")
+    public ResponseEntity<SchoolDto> findAllStudentsBySchoolId(@PathVariable("school-id") Long schoolId) {
+        return ResponseEntity.ok(service.findSchoolsWithStudents(schoolId));
     }
 }

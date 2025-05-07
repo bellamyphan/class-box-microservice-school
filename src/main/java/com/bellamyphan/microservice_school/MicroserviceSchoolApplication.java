26995065c958ccbdd83af972671c36aa0ea7aac0
BellamyPhan <bellamyphan@icloud.com>
2025-05-06 19:10:36 -0500

    Initial commit

---
+++ src/main/java/com/bellamyphan/microservice_school/MicroserviceSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.bellamyphan.microservice_school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bellamyphan/microservice_school/MicroserviceSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bellamyphan.microservice_school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

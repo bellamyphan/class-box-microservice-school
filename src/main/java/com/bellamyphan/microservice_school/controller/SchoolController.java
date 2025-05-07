cef643a9cdb92228f59c1cc6adcd106aa2255aa4
BellamyPhan <bellamyphan@icloud.com>
2025-05-06 20:25:11 -0500

    Create school controller class

---
+++ src/main/java/com/bellamyphan/microservice_school/controller/SchoolController.java
@@ -0,0 +1,29 @@
+package com.bellamyphan.microservice_school.controller;
+
+import com.bellamyphan.microservice_school.model.School;
+import com.bellamyphan.microservice_school.service.SchoolService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/schools")
+@RequiredArgsConstructor
+public class SchoolController {
+
+    private final SchoolService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void save(@RequestBody School school) {
+        service.saveSchool(school);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<School>> findAllSchools() {
+        return ResponseEntity.ok(service.findAllSchools());
+    }
+}

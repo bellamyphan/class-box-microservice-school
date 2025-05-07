9ae1080671bbd9630fd1b5910716715ea5cc6111
BellamyPhan <bellamyphan@icloud.com>
2025-05-06 20:23:50 -0500

    Create school model class

---
+++ src/main/java/com/bellamyphan/microservice_school/model/School.java
@@ -0,0 +1,25 @@
+package com.bellamyphan.microservice_school.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "schools")
+@Data // Lombok annotation to generate getters, setters, and other utility methods
+@NoArgsConstructor
+public class School {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false, unique = true)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    @NotEmpty
+    private String name;
+    @Column(name = "email", nullable = false)
+    @Email
+    private String email;
+}
